12dd5bd92fd61b476f2480248d7e10b2e4088ecd
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-04-03 02:19:47 +0300

    10 commit

--- src/js/cardTypes.js
+++ src/js/cardTypes.js
@@ -2,4 +2,8 @@ export const cardTypes = {
   visa: "visa",
   mastercard: "mastercard",
   mir: "mir",
+  american_express: "american-express",
+  diners: "diners",
+  discover: "discover",
+  jcb: "jcb"
 };
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   devServer: {
@@ -36,7 +37,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      },
+      }
     ],
   },
   plugins: [
@@ -48,5 +49,9 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/licenses.txt"}
+      ]})
   ],
 };
